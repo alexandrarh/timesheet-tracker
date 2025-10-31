10bd4d94f109135e4ac7b66b79283c5c71f0eb4a
alexandrarh <alexavndrarh@gmail.com>
2025-10-31 09:15:55 -0700

    Tested retrieving firm users

--- api_parts_test.py
+++ api_parts_test.py
@@ -25,6 +25,45 @@ def get_access_token(access_data: Dict) -> str:
     
     return access_token
 
+def get_firm_users(access_token: str) -> List[Dict] | int:
+    """Fetch all users associated with the firm."""
+    access_token = access_token
+    url = 'https://apps.timesolv.com/Services/rest/oauth2v1/firmUserSearch'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    payload = {
+        "OrderBy": "Id",
+        "SortOrderAscending": 0,
+        "PageSize": 100,
+        "PageNumber": 1,
+        "Criteria": [
+            {
+                "FieldName": "UserStatus",
+                "Operator": "=",
+                "Value": "Active"
+            }
+        ]
+    }
+
+    # Make the request
+    response = requests.post(url, headers=headers, json=payload)
+    
+    if response.status_code != 200:
+        print(f"Error fetching firm users: {response.status_code} - {response.text}")
+        return 1
+    
+    firm_json = json.dumps(response.json(), indent=2)
+    firm_list = json.loads(firm_json)['FirmUsers']
+    
+    return firm_list
+
+    # Check the result
+    # print(f"Status Code: {response.status_code}")
+    # print(f"Response: {json.dumps(response.json(), indent=2)}")
+
 def main():
     access_data = {
         "client_id": TIMESOLV_CLIENT_ID,
@@ -36,6 +75,10 @@ def main():
 
     access_token = get_access_token(access_data)
     # print(f"Access Token: {access_token}")
+
+    # Testing get all firm users
+    firm_users = get_firm_users(access_token)
+    print(firm_users)
 
 if __name__ == "__main__":
     main()
