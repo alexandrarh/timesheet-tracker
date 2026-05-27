5156e1962db77e9836cc1e672fd205640696e468
alexandrarh <alexavndrarh@gmail.com>
2026-05-27 10:42:50 -0700

    Update .gitignore and format

--- get_projects.py
+++ get_projects.py
@@ -20,8 +20,16 @@ def main():
         print(clients_response)
         return
 
+    client_data = pd.DataFrame(columns=['Id', 'ClientId', 'ClientName'])
+
     for client in clients_response:
-        print(f"Client ID: {client['Id']} - {client['ClientId']}, Name: {client['ClientName']}")
+        client_row = pd.DataFrame({
+            'Id': [client['Id']],
+            'ClientId': [client['ClientId']],
+            'ClientName': [client['ClientName']]
+        })
+        client_data = pd.concat([client_data, client_row], ignore_index=True)
+        # print(f"Client ID: {client['Id']} - {client['ClientId']}, Name: {client['ClientName']}")
 
     # Get all projects
     projects_response = api.get_project_details()
@@ -29,11 +37,22 @@ def main():
         print(projects_response)
         return
 
-    # project_data = pd.DataFrame(columns=['Id','ProjectId', 'ProjectName', 'ClientId'])
+    project_data = pd.DataFrame(columns=['Id','ProjectId', 'ProjectName', 'ClientId'])
 
-    print("Projects retrieved successfully:")
     for project in projects_response:
-        print(f"Project ID: {project['Id']}, Name: {project['ProjectId']} - {project['ProjectName']}. Belongs to Client ID: {project['ClientId']}")
+        project_row = pd.DataFrame({
+            'Id': [project['Id']],
+            'ProjectId': [project['ProjectId']],
+            'ProjectName': [project['ProjectName']],
+            'ClientId': [project['ClientId']]
+        })
+        project_data = pd.concat([project_data, project_row], ignore_index=True)
+        # print(f"Project ID: {project['Id']}, Name: {project['ProjectId']} - {project['ProjectName']}. Belongs to Client ID: {project['ClientId']}")
+
+    # Saving data to Excel file
+    with pd.ExcelWriter('client_project_data.xlsx') as writer:
+        client_data.to_excel(writer, sheet_name='Clients', index=False)
+        project_data.to_excel(writer, sheet_name='Projects', index=False)
 
 if __name__ == "__main__":
     main()
